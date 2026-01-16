a6d7ecacb06e627e0db408b80b568c9b3d41d576
arsenii <arseniy.kunilovskiy@gmail.com>
2026-01-16 11:12:48 -0500

    Arsenii

--- src/main/java/com/pluralsight/FinancialTracker2.java
+++ src/main/java/com/pluralsight/FinancialTracker2.java
@@ -20,8 +20,9 @@ public class FinancialTracker2 {
 
         Scanner scanner = new Scanner(System.in);
         boolean running = true;
-        //lskdgdddddd
-//ugggg
+
+
+
         while (running) {
             System.out.println("╔═══════════════════════════════════════╗");
             System.out.println("║       Welcome to TransactionApp       ║");
